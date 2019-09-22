4dc16977a68be24b8bb3f0a1a9cc307d64f69853
Ossi Myllymäki <omyllymaki@gmail.com>
2019-09-22 10:02:18 -0700

    Add debug option and testing to make_build script

--- scripts/make_build.sh
+++ scripts/make_build.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-rm -r -f build
-mkdir build
-cd build
-cmake ..
+if [ "$1" = "-d" ]
+then
+    echo "Building debug"
+    FOLDER_NAME="debug"
+    BUILD_COMMAND="cmake -DCMAKE_BUILD_TYPE=Debug .."
+else
+    echo "Building release"
+    FOLDER_NAME="build"
+    BUILD_COMMAND="cmake .."
+fi
+
+rm -r -f $FOLDER_NAME
+mkdir $FOLDER_NAME
+cd $FOLDER_NAME
+$BUILD_COMMAND
 make -j 8
+
+cd tests
+ctest
