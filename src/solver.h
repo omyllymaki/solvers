f4af1513b0366e5acb9e789278e2698ca65c72cf
Ossi Myllymäki <omyllymaki@gmail.com>
2019-09-28 09:03:08 -0700

    Add docstrings to solver interface

--- src/solver.h
+++ src/solver.h
@@ -4,16 +4,25 @@
 #include <iostream>
 #include <armadillo>
 
+//! Solver interface that needs to be inherited by individual solvers
+//! Solvers solves x from equations of the form f(x, L) = s
+//! s is observed signal and L is fixed set of coefficients
 class Solver
 {
 public:
 
+    //! Solves x, given signal
+    //! @param signal - observed signal
+    //! @returns solved x values
     virtual arma::mat solve(const arma::mat &signal) = 0;
 
+    //! Like solve but solves multiple signals at one time
     virtual std::vector<arma::mat> solve_multiple(const std::vector <arma::mat> &signals);
 
+    //! Estimated signal, calculated using solution x
     virtual arma::mat get_signal_estimate();
 
+    //! Difference between estimated and observed signal
     virtual arma::mat get_signal_residual();
 
 protected:
@@ -21,6 +30,9 @@ protected:
     arma::mat m_s;
     arma::mat m_x;
 
+    //! Signal model f
+    //! Function that calculates signal, given x and L
+    //! Abstract method that needs to be implemented by inheritors
     virtual arma::mat model(arma::mat x, arma::mat L) = 0;
 };
 
