4f0dffe38f801104a43f18d98e3f0f5bf5d24048
Ossi Myllymäki <omyllymaki@gmail.com>
2019-10-06 02:18:16 -0700

    Update GNSolver and unit test for it

--- src/gauss-newton/gn_solver.cpp
+++ src/gauss-newton/gn_solver.cpp
@@ -22,40 +22,81 @@ arma::mat GNSolver::solve(const arma::mat &s)
     m_x = randn(1, m_L.n_rows);
     m_s = s;
 
-    arma::mat gradient, inv_gradient, s_estimate, x, derivative, residual, residual0;
+    arma::mat s_estimate, residual;
 
     for (m_round = 0; m_round < m_max_iter; m_round++)
     {
+        update_residual();
+        update_gradient();
+        update_solution();
+        update_objective();
 
-       
-        s_estimate = model(m_x, m_L);
-        residual0 = s_estimate - m_s;
-
-        gradient.reset();
-        for (size_t i = 0; i < m_x.n_elem; i++)
+        if (is_termination_condition_filled())
         {
-            x = m_x;
-            x[i] = x[i] + m_x_delta;
-            s_estimate = model(x, m_L);
-            residual = s_estimate - m_s;
-            derivative = (residual - residual0) / m_x_delta;
-            gradient.insert_rows(i, derivative);
+            cout << "Change in objective value smaller than specified threshold" << endl;
+            cout << "Iteration terminated at round " << m_round << endl;
+            return m_x;
         }
-        inv_gradient = calculate_svd_inverse(gradient.t());
-        arma::mat step = inv_gradient * residual0.t();
-
-        m_x = m_x - step.t();
-
-        // cout << m_x << endl;
 
         m_objective_prev = m_objective;
     }
 
     return m_x;
-
 }
 
 arma::mat GNSolver::model(mat x, mat L)
 {
     return x * L;
+}
+
+arma::mat GNSolver::objective(arma::mat estimate, arma::mat expected)
+{
+    return rmse(estimate, expected);
+}
+
+void GNSolver::update_solution()
+{
+    arma::mat inv_gradient = calculate_svd_inverse(m_gradient.t());
+    arma::mat step = inv_gradient * m_residual.t();
+    m_x = m_x - step.t();
+}
+
+void GNSolver::update_gradient()
+{
+    m_gradient.reset();
+    arma::mat x, s_estimate, residual, derivative;
+    for (size_t i = 0; i < m_x.n_elem; i++)
+    {
+        x = m_x;
+        x[i] = x[i] + m_x_delta;
+        s_estimate = model(x, m_L);
+        residual = s_estimate - m_s;
+        derivative = (residual - m_residual) / m_x_delta;
+        m_gradient.insert_rows(i, derivative);
+    }
+}
+
+bool GNSolver::is_termination_condition_filled()
+{
+    mat rel_obj_change = (m_objective_prev - m_objective) / m_objective;
+    if (as_scalar(rel_obj_change) < m_termination_threshold)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void GNSolver::update_objective()
+{
+    arma::mat s_estimate = model(m_x, m_L);
+    m_objective = objective(s_estimate, m_s);
+}
+
+void GNSolver::update_residual()
+{
+    arma::mat s_estimate = model(m_x, m_L);
+    m_residual = s_estimate - m_s;
 }
--- src/gauss-newton/gn_solver.h
+++ src/gauss-newton/gn_solver.h
@@ -10,10 +10,22 @@ protected:
     double m_termination_threshold;
     int m_max_iter;
     size_t m_round;
-    const double m_x_delta = 0.1;
-    arma::mat m_objective_prev, m_objective, m_gradient;
+    const double m_x_delta = 0.000001;
+    arma::mat m_objective_prev, m_objective, m_gradient, m_residual;
 
     virtual arma::mat model(arma::mat x, arma::mat L) override;
+
+    virtual void update_solution();
+
+    virtual void update_gradient();
+
+    virtual bool is_termination_condition_filled();
+
+    virtual void update_objective();
+
+    virtual void update_residual();
+
+    virtual arma::mat objective(arma::mat estimate, arma::mat expected);
 
 public:
     GNSolver(const arma::mat &L,
