258bb3c00879745375ae6a74a04e231db752df97
Ossi Myllymäki <omyllymaki@gmail.com>
2019-10-20 01:59:47 -0700

    Add SolverTester and use it in ls_solver_test

--- tests/ls_solver_test.cpp
+++ tests/ls_solver_test.cpp
@@ -8,30 +8,10 @@
 
 INITIALIZE_EASYLOGGINGPP
 
-BOOST_FIXTURE_TEST_SUITE(s, TestFixture<LSSolver>);
+LSSolver solver;
 
-BOOST_AUTO_TEST_CASE(fit_only_positive_values)
+BOOST_AUTO_TEST_CASE(test_common_cases)
 {
-    mat weights = {1, 1, 2};
-    test_solve(weights);
+    auto tester = SolverTester<LSSolver>(solver);
+    tester.test_common();
 }
-
-BOOST_AUTO_TEST_CASE(fit_zero_values)
-{
-    mat weights = {0, 0, 0};
-    test_solve(weights);
-}
-
-BOOST_AUTO_TEST_CASE(fit_positive_and_negative_values)
-{
-    mat weights = {-1, 1, 2};
-    test_solve(weights);
-}
-
-BOOST_AUTO_TEST_CASE(fit_only_negative_values)
-{
-    mat weights = {-1, -1, -2};
-    test_solve(weights);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
