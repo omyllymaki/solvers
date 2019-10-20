9edbaf99f3db93df2369685023fc1cc6e2cefbd1
Ossi Myllymäki <omyllymaki@gmail.com>
2019-10-20 00:32:09 -0700

    Add TestFixture and use it in ls_solver_test

--- tests/ls_solver_test.cpp
+++ tests/ls_solver_test.cpp
@@ -8,39 +8,30 @@
 
 INITIALIZE_EASYLOGGINGPP
 
-using namespace arma;
-
-mat signals = create_signals();
-auto solver = LSSolver(signals);
+BOOST_FIXTURE_TEST_SUITE(s, TestFixture<LSSolver>);
 
 BOOST_AUTO_TEST_CASE(fit_only_positive_values)
 {
     mat weights = {1, 1, 2};
-    mat signal = sum_signal(weights, signals);
-    mat result = solver.solve(signal);
-    BOOST_CHECK(is_equal(weights, result));                                       
+    test_solve(weights);
 }
 
 BOOST_AUTO_TEST_CASE(fit_zero_values)
 {
     mat weights = {0, 0, 0};
-    mat signal = sum_signal(weights, signals);
-    mat result = solver.solve(signal);
-    BOOST_CHECK(is_equal(weights, result));                                        
+    test_solve(weights);
 }
 
 BOOST_AUTO_TEST_CASE(fit_positive_and_negative_values)
 {
     mat weights = {-1, 1, 2};
-    mat signal = sum_signal(weights, signals);
-    mat result = solver.solve(signal);
-    BOOST_CHECK(is_equal(weights, result));                                          
+    test_solve(weights);
 }
 
 BOOST_AUTO_TEST_CASE(fit_only_negative_values)
 {
     mat weights = {-1, -1, -2};
-    mat signal = sum_signal(weights, signals);
-    mat result = solver.solve(signal);
-    BOOST_CHECK(is_equal(weights, result));                                          
+    test_solve(weights);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -20,3 +20,30 @@ bool is_equal(mat x, mat y, float threshold = 0.001)
 {
     return approx_equal(x, y, "absdiff", threshold);
 }
+
+template <typename T>
+struct TestFixture
+{
+    mat m_signals;
+    T m_solver;
+
+    TestFixture()
+    {
+        m_signals = create_signals();
+        m_solver.set_library(m_signals);
+    }
+
+    ~TestFixture()
+    {
+    }
+
+    void test_solve(mat weights, float tolerance = 0.001)
+    {
+        mat signal = sum_signal(weights, m_signals);
+        mat result = m_solver.solve(signal);
+        BOOST_TEST_MESSAGE(boost::unit_test::framework::current_test_case().p_name);
+        BOOST_TEST_MESSAGE("Expected: " << weights);
+        BOOST_TEST_MESSAGE("Actual: " << result);
+        BOOST_CHECK(is_equal(weights, result, tolerance));
+    }
+};
