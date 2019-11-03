571dc13a36602d1f5375c19b6808cadc132ad2df
Ossi Myllymäki <omyllymaki@gmail.com>
2019-11-02 23:42:10 -0700

    Improve Solver interface documentation

--- src/solver.h
+++ src/solver.h
@@ -7,37 +7,39 @@
 
 using model_wrapper = std::function<arma::mat(arma::mat, arma::mat)>;
 
-//! Base Solver class
-//! Provides interface and some common methods for all solvers
-//! Solvers solves x from equations of the form f(x, L) = s
-//! s is observed signal, L is fixed set of coefficients and function f is signal model
+//! Base Solver class.
+//! Provides interface and some common methods for all solvers.
+//! Solvers solves x from equations of the form f(x, L) = s.
+//! s is observed signal.
+//! L is library which is fixed set of coefficients.
+//! function f is signal model.
 class Solver
 {
 public:
-    //! Solves x, given signal
+    //! Solves x, given signal.
     //! @param signal - observed signal
     //! @returns solved x values
     virtual arma::mat solve(const arma::mat &signal) = 0;
 
-    //! Like solve but solves multiple signals at one time
+    //! Like solve but solves multiple signals at one time.
     virtual std::vector<arma::mat> solve_multiple(const std::vector<arma::mat> &signals);
 
-    //! Estimated signal, calculated using solution x
+    //! Estimated signal, calculated using solution x.
     virtual arma::mat get_signal_estimate();
 
-    //! Difference between estimated and observed signal
+    //! Difference between estimated and observed signal.
     virtual arma::mat get_signal_residual();
 
-    //! Set library L in f(x, L) = s
+    //! Set library L in f(x, L) = s.
     virtual void set_library(arma::mat L);
 
-    //! get library L in f(x, L) = s
+    //! get library L in f(x, L) = s.
     arma::mat get_library();
 
-    //! Set signal s in f(x, L) = s
+    //! Set signal s in f(x, L) = s.
     virtual void set_signal(arma::mat s);
 
-    //! Get model f in f(x, L) = s
+    //! Get model f in f(x, L) = s.
     model_wrapper get_model();
 
 protected:
@@ -46,7 +48,7 @@ protected:
     arma::mat m_x;
     model_wrapper model = linear_model; // Default model
 
-    //! Linear signal model, f = x*L
+    //! Linear signal model, f = x*L.
     static arma::mat linear_model(arma::mat x, arma::mat L);
 };
 
