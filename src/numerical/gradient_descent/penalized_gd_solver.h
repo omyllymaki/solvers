b857dcd86ccc1e350ce0ba46e8cd6f2a86c1fc7d
Ossi Myllymäki <omyllymaki@gmail.com>
2019-11-24 08:34:58 -0800

    Add docstrings to PenalizedGDSolver

--- src/numerical/gradient_descent/penalized_gd_solver.h
+++ src/numerical/gradient_descent/penalized_gd_solver.h
@@ -3,6 +3,9 @@
 
 #include "gd_solver.h"
 
+//! Gradient descent solver with penalty.
+//! Penalty is added to objective function in order to constrain fit.
+//! In this class, penalty is given for negative values.
 class PenalizedGDSolver : public GDSolver
 {
 
@@ -12,6 +15,12 @@ private:
 public:
     PenalizedGDSolver(){};
 
+    //! Solver initialization.
+    //! @param L - Library used to fit model f(x,L) = s.
+    //! @param lr - Learning rate. This is the main tuning parameter and affects to convergence and optimization time. 
+    //! @param max_iter - Maximum number of iterations.
+    //! @param termination_threshold - Threshold value for objective. The iteration is terminated when threshold value is reached.
+    //! @param penalty - Amount of penalty given for negative values. 
     PenalizedGDSolver(const arma::mat &L,
                    const double lr = 100.0,
                    const int max_iter = 10000,
